f0df7eb9f809f444eb8dc917c88fa516ad0a7687
rubgitgit <42901795+rubgitgit@users.noreply.github.com>
2021-04-13 09:48:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.get("/expenses", requiresAuth(), async (req, res, next) => {
     expenses,
   });
 });
-
+//
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
